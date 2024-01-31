4cfaf5a4ca8fa619481eefa8ee69b0c9518327ea
Disk-MTH <gillet.mat@free.fr>
2024-01-31 18:32:00 +0100

    setup first relation with ORM

--- src/model/User.php
+++ src/model/User.php
@@ -2,52 +2,55 @@
 
 namespace stagify\model;
 
-use DateTimeImmutable;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\OneToOne;
 use Doctrine\ORM\Mapping\Table;
 
-#[Entity, Table(name: 'users')]
-final class User
+#[Entity, Table(name: "user")]
+class User
 {
-    #[Id, Column(type: 'integer'), GeneratedValue(strategy: 'AUTO')]
+    #[Id, Column(name: "user_id", type: "integer"), GeneratedValue(strategy: "AUTO")]
     private int $id;
 
-    #[Column(type: 'string', length: 255)]
-    private string $name;
+    #[Column(name: "first_name", type: "string", nullable: false)]
+    private string $firstName;
 
-    #[Column(type: 'string', unique: true, nullable: false)]
-    private string $email;
+    #[Column(name: "last_name", type: "string", nullable: false)]
+    private string $lastName;
 
-    #[Column(type: 'datetimetz_immutable', nullable: false)]
-    private DateTimeImmutable $registeredAt;
+    #[ManyToOne(targetEntity: Auth::class), JoinColumn(name: "user_id", referencedColumnName: "auth_id")]
+    private Auth $auth;
 
-    public function __construct(string $name, string $email)
+    //#[ManyToOne(targetEntity: Campus::class), JoinColumn(name: "user_id", referencedColumnName: "id")]
+    //private Campus $campus;
+
+    public function __construct(string $firstName, string $lastName, string $login, string $passwordHash)
     {
-        $this->name = $name;
-        $this->email = strtolower($email);
-        $this->registeredAt = new DateTimeImmutable("now");
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->auth = new Auth($login, $passwordHash);
     }
 
-    public function getId(): int
+    public function getFirstName(): string
     {
-        return $this->id;
+        return $this->firstName;
     }
 
-    public function getName(): string
+    public function getLastName(): string
     {
-        return $this->name;
+        return $this->lastName;
     }
 
-    public function getEmail(): string
+    public function getAuth(): Auth
     {
-        return $this->email;
-    }
-
-    public function getRegisteredAt(): DateTimeImmutable
-    {
-        return $this->registeredAt;
+        return $this->auth;
     }
 }
