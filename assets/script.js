d058f21093b9d60765a10d8f59c7aac1feff63ec
DiskMTH <gillet.mat@free.fr>
2024-03-18 20:11:05 +0100

    add pwa

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,7 @@
 window.addEventListener("load", function () {
     if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/service-worker.js").then(function (registration) {
-            console.log("Service Worker registered with scope:", registration.scope);
-        }).catch(function (error) {
-            console.log("Service Worker registration failed:", error);
+        navigator.serviceWorker.register("/assets/service-worker.js").catch(function (error) {
+            console.error("Service Worker registration failed with " + error);
         });
     }
 });
