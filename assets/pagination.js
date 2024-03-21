80c8e8107406ac33d981c29e89ca55e25fe96e8a
DiskMTH <gillet.mat@free.fr>
2024-03-21 12:56:45 +0100

    finish pagination script

--- assets/pagination.js
+++ assets/pagination.js
@@ -1,14 +1,14 @@
-(function($, window) {
+(function($) {
     $.fn.pagination = function(options) {
-        var $owner = this,
+        const $owner = this,
             settings = $.extend({
-                    count: 0, //total pages
-                    page: 1, //active page
-                    maxVisible: null, //max visible pages at the same time
-                    href: 'javascript:void({{number}});', //href for each page
-                    hrefVariable: '{{number}}', //variable for href
-                    previous: 'Previous', //previous page lot icon
-                    next: 'Next', //next page lot icon
+                    page: 1,
+                    count: 0,
+                    maxVisible: null,
+                    href: "javascript:void({{number}});",
+                    hrefVariable: "{{number}}",
+                    previous: "&laquo;",
+                    next: "&raquo;",
                 },
                 $owner.data('settings') || {},
                 options || {});
@@ -18,47 +18,49 @@
         if (!settings.maxVisible || isNaN(settings.maxVisible)) settings.maxVisible = settings.count;
         if (settings.maxVisible < 1 || settings.maxVisible > settings.count) settings.maxVisible = settings.count;
 
-        function href(c) {
-            return settings.href.replace(settings.hrefVariable, c);
+        function href(page) {
+            return settings.href.replace(settings.hrefVariable, page);
         }
 
         return this.each(function() {
-            var $container = $(this);
-            var start = settings.page - Math.floor(settings.maxVisible / 2);
+            const $container = $(this);
+            let start = settings.page - Math.floor(settings.maxVisible / 2);
             start = Math.max(start, 1);
             start = Math.min(start, 1 + settings.count - settings.maxVisible);
-            var end = Math.min(start + settings.maxVisible - 1, settings.count);
+            const end = Math.min(start + settings.maxVisible - 1, settings.count);
 
             $container.empty();
             $container.append('<ul class="pagination"></ul>');
-            var $pagination = $container.find('.pagination');
+            const $pagination = $container.find('.pagination');
 
             if (settings.maxVisible < settings.count) {
-                var prevClass = settings.page === 1 ? 'page-item disabled' : 'page-item';
-                $pagination.append('<li class="' + prevClass + '"><a class="page-link prev-page" href="' + href(settings.page - 1) + '">' + settings.prevIcon + '</a></li>');
+                const previousClass = settings.page === 1 ? 'page-item disabled' : 'page-item';
+                $pagination.append('<li class="' + previousClass + '"><a class="page-link previous-page" href="' + href(settings.page - 1) + '">' + settings.previous + '</a></li>');
             }
 
             for (var i = start; i <= end; i++) {
-                var className = i === settings.page ? 'page-item active' : 'page-item';
-                $pagination.append('<li class="' + className + '"><a class="page-link" href="' + href(i) + '">' + i + '</a></li>');
+                const pageClass = i === settings.page ? 'page-item active' : 'page-item';
+                $pagination.append('<li class="' + pageClass + '"><a class="page-link" href="' + href(i) + '">' + i + '</a></li>');
             }
 
             if (settings.maxVisible < settings.count) {
-                var nextClass = settings.page === settings.count ? 'page-item disabled' : 'page-item';
-                $pagination.append('<li class="' + nextClass + '"><a class="page-link next-page" href="' + href(settings.page + 1) + '">' + settings.nextIcon + '</a></li>');
+                const nextClass = settings.page === settings.count ? 'page-item disabled' : 'page-item';
+                $pagination.append('<li class="' + nextClass + '"><a class="page-link next-page" href="' + href(settings.page + 1) + '">' + settings.next + '</a></li>');
             }
 
-            $pagination.find('.page-link').not('.prev-page, .next-page').click(function(e) {
+            $pagination.find('.page-link').not('.previous-page, .next-page').click(function(e) {
                 e.preventDefault();
                 settings.page = parseInt($(this).text(), 10);
                 $owner.pagination(settings);
+                $owner.trigger("changePage", [settings.page]);
             });
 
-            $pagination.find('.prev-page').click(function(e) {
+            $pagination.find('.previous-page').click(function(e) {
                 e.preventDefault();
                 if (settings.page > 1) {
                     settings.page--;
                     $owner.pagination(settings);
+                    $owner.trigger("changePage", [settings.page]);
                 }
             });
 
@@ -67,8 +69,9 @@
                 if (settings.page < settings.count) {
                     settings.page++;
                     $owner.pagination(settings);
+                    $owner.trigger("changePage", [settings.page]);
                 }
             });
         });
     }
-})(jQuery, window);
+})(jQuery);
