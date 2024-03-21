31b5258437944d6fdbcbed49cdc25229827ac74c
DiskMTH <gillet.mat@free.fr>
2024-03-21 12:39:04 +0100

    WIP pagination

--- assets/pagination.js
+++ assets/pagination.js
@@ -5,10 +5,10 @@
                     count: 0, //total pages
                     page: 1, //active page
                     maxVisible: null, //max visible pages at the same time
-                    href: 'javascript:void(0);', //href for each page
+                    href: 'javascript:void({{number}});', //href for each page
                     hrefVariable: '{{number}}', //variable for href
-                    nextIcon: 'Next', //next page lot icon (a lot of page is maxVisible)
-                    prevIcon: 'Previous', //previous page lot icon
+                    previous: 'Previous', //previous page lot icon
+                    next: 'Next', //next page lot icon
                 },
                 $owner.data('settings') || {},
                 options || {});
@@ -16,6 +16,7 @@
         if(settings.count <= 0) return this;
         if (settings.page < 1) settings.page = 1;
         if (!settings.maxVisible || isNaN(settings.maxVisible)) settings.maxVisible = settings.count;
+        if (settings.maxVisible < 1 || settings.maxVisible > settings.count) settings.maxVisible = settings.count;
 
         function href(c) {
             return settings.href.replace(settings.hrefVariable, c);
@@ -32,8 +33,9 @@
             $container.append('<ul class="pagination"></ul>');
             var $pagination = $container.find('.pagination');
 
-            if (settings.page > 1) {
-                $pagination.append('<li class="page-item"><a class="page-link" href="' + href(settings.page - 1) + '">' + settings.prevIcon + '</a></li>');
+            if (settings.maxVisible < settings.count) {
+                var prevClass = settings.page === 1 ? 'page-item disabled' : 'page-item';
+                $pagination.append('<li class="' + prevClass + '"><a class="page-link prev-page" href="' + href(settings.page - 1) + '">' + settings.prevIcon + '</a></li>');
             }
 
             for (var i = start; i <= end; i++) {
@@ -41,14 +43,31 @@
                 $pagination.append('<li class="' + className + '"><a class="page-link" href="' + href(i) + '">' + i + '</a></li>');
             }
 
-            if (settings.page < settings.count) {
-                $pagination.append('<li class="page-item"><a class="page-link" href="' + href(settings.page + 1) + '">' + settings.nextIcon + '</a></li>');
+            if (settings.maxVisible < settings.count) {
+                var nextClass = settings.page === settings.count ? 'page-item disabled' : 'page-item';
+                $pagination.append('<li class="' + nextClass + '"><a class="page-link next-page" href="' + href(settings.page + 1) + '">' + settings.nextIcon + '</a></li>');
             }
 
-            $pagination.find('.page-link').click(function(e) {
+            $pagination.find('.page-link').not('.prev-page, .next-page').click(function(e) {
                 e.preventDefault();
                 settings.page = parseInt($(this).text(), 10);
                 $owner.pagination(settings);
+            });
+
+            $pagination.find('.prev-page').click(function(e) {
+                e.preventDefault();
+                if (settings.page > 1) {
+                    settings.page--;
+                    $owner.pagination(settings);
+                }
+            });
+
+            $pagination.find('.next-page').click(function(e) {
+                e.preventDefault();
+                if (settings.page < settings.count) {
+                    settings.page++;
+                    $owner.pagination(settings);
+                }
             });
         });
     }
