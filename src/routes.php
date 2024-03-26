95091f506090b93cf838e9c8409ef01878959e14
DiskMTH <gillet.mat@free.fr>
2024-03-26 13:45:04 +0100

    patch login

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,7 @@
 
 namespace stagify;
 
+use DateTime;
 use Doctrine\ORM\EntityManager;
 use Exception;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -10,10 +11,9 @@ use Psr\Log\LoggerInterface as Logger;
 use Slim\App;
 use Slim\Psr7\UploadedFile;
 use Slim\Views\Twig;
-use stagify\Model\Entities\Company;
-use stagify\Model\Entities\InternshipOffer;
-use stagify\Model\Repositories\CompanyRepo;
-use stagify\Model\Repositories\InternshipOfferRepo;
+use stagify\Middlewares\FlashMiddleware;
+use stagify\Model\Entities\Session;
+use stagify\Model\Entities\User;
 
 /**
  * @throws Exception
@@ -23,14 +23,10 @@ function render(Response $response, string $template, array $data = []): Respons
     global $twig;
     global $entityManager;
 
-    /*$sessionRepo = $entityManager->getRepository(Session::class);
+    $sessionRepo = $entityManager->getRepository(Session::class);
     $session = $sessionRepo->findOneBy(["token" => $_COOKIE["session"] ?? ""]);
 
     if ($session != null) {
-        if ($template !== "pages/login.twig") {
-            $data["user"] = $entityManager->getRepository(User::class)->findOneBy(["id" => $_SESSION["user"]]);
-        }
-
         if ($session->getLastActivity() < new DateTime("-" . Session::$duration)) {
             $session = null;
             Session::logOut();
@@ -46,7 +42,11 @@ function render(Response $response, string $template, array $data = []): Respons
         return redirect($response, "login");
     } else if ($session != null && $template === "pages/login.twig") {
         return redirect($response, "/");
-    }*/
+    }
+
+    if ($session != null) {
+        $data["user"] = $entityManager->getRepository(User::class)->findOneBy(["id" => $_SESSION["user"]]);
+    }
 
     return $twig->render($response, $template, $data);
 }
@@ -56,6 +56,7 @@ function redirect(Response $response, string $url): Response
     return $response->withStatus(302)->withHeader("Location", $url);
 }
 
+//todo: remake
 function moveUploadedFile($directory, UploadedFile $uploadedFile): string
 {
     $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
@@ -78,7 +79,7 @@ return function (App $app, Logger $logger, Twig $twig, EntityManager $entityMana
 
     $app->get("/offline", function (Request $request, Response $response) use ($twig) {
         return $twig->render($response, "pages/offline.twig");
-    });
+    })->setName("offline");
 
     $session = require __DIR__ . "/Routes/session.php";
     $session($app, $logger, $twig, $entityManager);
