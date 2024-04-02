8d4c38af27ab9ba900f5aa86ca7bcdf8879fdcd8
OddOrigin <julien.turcot@icloud.com>
2024-04-02 14:11:48 +0200

    companies filter missing employee and interns count

--- src/Model/Repositories/CompanyRepo.php
+++ src/Model/Repositories/CompanyRepo.php
@@ -11,16 +11,38 @@ use Throwable;
 
 final class CompanyRepo extends EntityRepository
 {
-    function pagination(int $page, string|null $rating, string|null $internshipsCount, string|null $internsCount, int|null $employeesCountLow, int|null $employeesCountHigh, bool $count, int $limit = 12): array|int
+    function pagination(int $page, string|null $rating, string|null $internshipsCount, string|null $internsCount, string|null $employeesCount, bool $count, int $limit = 12): array|int
     {
         //TODO: apply filters
-        $builder = $this->createQueryBuilder("c")
-            ->select("c.id, c.name, l.city, l.zipCode, c.logoPath, cio.id, cio.title")
+        $builder = $this
+            ->createQueryBuilder("c")
+            ->select("c.id, c.name, l.city, l.zipCode, c.logoPath, cio.id, cio.title, c.employeeCount")
             ->addSelect("COUNT(ir.id) AS numberOfReviews")
+            ->addSelect("AVG(ir.grade) AS averageRating")
             ->innerJoin("c.internships", "cio")
             ->innerJoin("cio.location", "l")
             ->leftJoin("cio.rates", "ir")
             ->groupBy("c.id, c.name, l.city, l.zipCode, c.logoPath, cio.title, cio.id");
+
+        if ($rating) $builder->orderBy("averageRating", $rating);
+
+        if ($internshipsCount) $builder->orderBy("COUNT(cio.id)", $internshipsCount);
+
+        if ($internsCount) $builder->addSelect("COUNT(DISTINCT i.id) AS internsCount")
+            ->innerJoin("cio.interns", "i")
+            ->groupBy("c.id, c.name, l.city, l.zipCode, c.logoPath, cio.title, cio.id")
+            ->orderBy("internsCount", $internsCount);
+
+        if ($employeesCount) {
+            $limits = explode("_", $employeesCount);
+            if ($limits[0] != 0) $builder
+                ->andWhere("c.employeeCount >= :minEmployees")
+                ->setParameter("minEmployees", $limits[0]);
+            if ($limits[1] != 0) $builder
+                ->andWhere("c.employeeCount <= :maxEmployees")
+                ->setParameter("maxEmployees", $limits[1]);
+        }
+
 
         if ($count) {
             try {
