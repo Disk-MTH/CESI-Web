191c5c99c2283dfcd281bcbc75dc00826d4a6e7f
DiskMTH <gillet.mat@free.fr>
2024-04-03 11:58:46 +0200

    patch password filed with FA

--- assets/script.js
+++ assets/script.js
@@ -11,20 +11,6 @@ function navigateTo(event, url) {
     event.stopPropagation();
 }
 
-function togglePasswordField(event, id) {
-    const element = document.getElementById(id);
-    const icon = document.getElementById(id + "-icon");
-
-    if (element.type === "password") {
-        element.type = "text";
-        icon.src = "/assets/svg/misc/visibility_off.svg";
-
-    } else {
-        element.type = "password";
-        icon.src = "/assets/svg/misc/visibility_on.svg";
-    }
-}
-
 function toggleWishMark(event, id) {
     const icon = document.getElementById(id);
 
